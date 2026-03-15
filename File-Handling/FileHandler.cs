931c5bfaa9cd22032c331f5956872a653c0d5125
PavanKumar2621 <madempavankumar2621@gmail.com>
2026-03-15 16:30:58 +0530

    Add project files.

---
+++ File-Handling/FileHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Handling
+{
+    internal class FileHandler
+    {
+        public void StoreDataInFile(FileStream fsd, string line)
+        {           
+            using (StreamWriter sw = new StreamWriter(fsd))
+            {
+                sw.WriteLine(line);
+            }            
+        }
+        public string RetriveDataInFile(FileStream fsd)
+        {
+            string? line = null;
+            using (StreamReader sw = new StreamReader(fsd))
+            {
+                line = sw.ReadToEnd();
+            }
+            return line;
+        }
+        public void StoreBinaryDataInFile(FileStream fsd, string s, int val, float fval)
+        {
+            using (BinaryWriter bw = new BinaryWriter(fsd))
+            {
+                if (s != null)
+                    bw.Write(s);
+                bw.Write(val);
+                bw.Write(fval);
+            }
+        }
+        public string RetirveBinaryDataInFile(FileStream fsd)
+        {
+            string? s = null;
+            int val = 0;
+            float fval = 0;
+            using (BinaryReader br = new BinaryReader(fsd))
+            {
+                s = br.ReadString();
+                val = br.ReadInt32();
+                fval = br.ReadSingle();
+            }
+            return $"Retrived File Data:\nString: {s}\nInteger: {val}\nFloat: {fval}";
+        }
+    }
+}
---
+++ File-Handling/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using File_Handling;
+using System.Collections;
+
+Console.WriteLine("Hello, World!");
+var filewrite = new FileHandler();
+
+while (true)
+{
+    Console.WriteLine("---------------------------------------------");
+    Console.WriteLine("Welcome to File Handling Concept");
+    Console.WriteLine("Select which option You want\n1. Write Date to File.\n2. Read Data From File." +
+        "\n3. Write Date to a File to Save in Binary Format.\n4. Retrive Data From File Which is in Binary Format." +
+        "\n5. Exit Program");    
+
+    int option = Convert.ToInt16(Console.ReadLine());
+    switch (option)
+    {
+        case 1: call1(); break;
+        case 2: call2(); break;
+        case 3: call3(); break;
+        case 4: call4(); break;
+        case 5: Console.WriteLine("Program Exiting..."); return;
+        default: Console.WriteLine("Wrong Selection."); break;
+    }
+}
+
+void call1()
+{
+    Console.WriteLine("Enter a Line to be append to a File");
+    string? line = Console.ReadLine();
+    FileStream fsw = new FileStream("WriteFile.txt", FileMode.Append, FileAccess.Write);
+    if (line != null)
+        filewrite.StoreDataInFile(fsw, line);
+    Console.WriteLine("Data Written in File Successfully.");
+}
+void call2()
+{
+    FileStream fsr = new FileStream("WriteFile.txt", FileMode.Open, FileAccess.Read);
+    string? line = filewrite.RetriveDataInFile(fsr);
+    Console.WriteLine($"Retrived File Data\n {line}");
+}
+
+void call3() 
+{       
+    Console.WriteLine("Enter a String to be append to a File");
+    string? s = Console.ReadLine();
+    Console.WriteLine("Enter an Integer value to be append to a File");
+    int val = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter a Float value to be append to a File");
+    float fval = Convert.ToSingle(Console.ReadLine());
+
+    FileStream fsr = new FileStream("Binary.txt", FileMode.Create, FileAccess.Write);
+    if (s != null)
+        filewrite.StoreBinaryDataInFile(fsr, s, val, fval);    
+    Console.WriteLine("Data Written in File Successfully.");
+}
+void call4()
+{
+    FileStream fsr = new FileStream("Binary.txt", FileMode.Open, FileAccess.Read);
+    string s = filewrite.RetirveBinaryDataInFile(fsr);
+    Console.WriteLine(s);
+}
